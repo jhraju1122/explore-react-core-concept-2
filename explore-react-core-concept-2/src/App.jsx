0398efb47228f42f74018f84fdd90561dc6f157c
jhraju <jhraju890@gmail.com>
2024-03-10 07:04:56 +0600

    added number add reduce by react

---
+++ explore-react-core-concept-2/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Cou from './Cou'
+//  import Counter from './Counter'
+function App() {
+  function  handleClick() {
+    alert('button clicked');
+  }
+   const handleClick2 = () =>{
+    alert('button clicked');
+
+    
+   }
+
+  return (
+    <>
+   
+
+      <h1>React core concept 2</h1>
+       <button onClick={handleClick}>Click me</button>
+
+       <button onClick={handleClick2}>Click me</button>
+       <Cou></Cou>
+      {/* <Counter></Counter> */}
+    </>
+  )
+}
+export default App
